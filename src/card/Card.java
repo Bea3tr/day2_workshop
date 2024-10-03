47c3931b8fca65096c74203f9499e3bf100cb957
Beatrice <beatricesim31@gmail.com>
2024-10-03 19:10:18 +0800

    created shuffle & draw

--- src/card/Card.java
+++ src/card/Card.java
@@ -5,20 +5,20 @@ public class Card {
     private final String name;
     private final String suit;
     private final int value;
-
+     
     // You cannot create a blank card
     public Card(String suit, String name, int value) {
-        this.name = name;
         this.suit = suit;
+        this.name = name;
         this.value = value;
     }
-
-    public String getName() {return name;}
-    public String getSuit() {return suit;}
-    public int getValue() {return value;}
-
+     
+    public String getName() { return name; }
+    public String getSuit() { return suit; }
+    public int getValue() { return value; }
+     
     @Override
     public String toString() {
-        return "Card{Suit: %s, Name: %s, Value: %d}".formatted(suit, name, value);
-    }   
+        return "Card{suit: %s, name: %s, value: %d}".formatted(suit, name, value);
+    }
 }
--- src/card/CardDeck.java
+++ src/card/CardDeck.java
@@ -7,11 +7,14 @@ import java.io.Console;
 public class CardDeck {
 
     public static final String[] SUIT = {"Diamond", "Spade", "Clubs", "Hearts"};
-    public static final String[] NAME = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
-                                        "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-    public static final int[] VALUE = {1, 2, 3, 4, 5, 6, 7, 
-                                        8, 9, 10, 10, 10, 10};
-    private static final int NUM_CARD = 52;
+    public static final String[] NAME = {
+        "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", 
+        "Eight", "Nine", "Ten", "Jack", "Queen", "King"
+    };
+    public static final int[] VALUE = {
+        1, 2, 3, 4, 5, 6, 7, 
+        8, 9, 10, 10, 10, 10
+    };
     protected Card[] cards;
 
     public CardDeck() {
@@ -38,38 +41,48 @@ public class CardDeck {
         return this.cards[pos];
     }
 
-    public ArrayList<Integer> shuffle() {
+    public Card[] shuffle(Card[] cards) {
+
+        /* Using Collections.shuffle 
+         * List<Card> cardsList = Arrays.asList(cards);
+         * Collections.shuffle(cardsList);
+         * Card[] shuffledDeck = cardsList.toArray(new Card[52]);
+        */
         
         Random rand = new SecureRandom();
-        ArrayList<Integer> deck =new ArrayList<Integer>();
-        for (int i=0; i < NUM_CARD; i++) {
-            deck.add(i);
-        }
-
-        ArrayList<Integer> shuffledDeck = new ArrayList<Integer>();
-        while (deck.size() > 0) {
-            int idx = rand.nextInt(deck.size());
-            shuffledDeck.add(deck.get(idx));
-            deck.remove(idx);
+        Card[] shuffledDeck = new Card[52];
+        ArrayList<Integer> randList = new ArrayList<Integer>();
+        
+        for (int i = 0; i < cards.length; i++) {
+            int idx = rand.nextInt(cards.length);
+            while (randList.contains(idx)){
+                idx = rand.nextInt(cards.length);
+            }
+            randList.add(idx);
+            shuffledDeck[i] = cards[idx];
         }
         return shuffledDeck;
     }
 
-    public void draw(ArrayList<Integer> deck) {
+    public void draw(Card[] cards) {
 
         Console cons = System.console();
         boolean isStop = false;
+        ArrayList<Integer> randList = new ArrayList<Integer>();
 
         while (!isStop){
             System.out.println("Please draw a card");
             Random rand = new SecureRandom();
-            int drawn = rand.nextInt(deck.size());
+            int drawn = rand.nextInt(cards.length);
+            while (randList.contains(drawn)) {
+                drawn = rand.nextInt(cards.length);
+            }
 
-            System.out.printf("The card you have drawn is: %d\n", deck.get(drawn));
-            deck.remove(drawn);
+            System.out.printf("The card you have drawn is: %d\n", cards[drawn]);
 
             String input = cons.readLine("Do you wish to draw another card? ");
-            if (input.equals("no") || deck.size() == 0){
+
+            if (input.equals("no") || randList.size() == cards.length){
                 isStop = true;
             }
          }
